4acfc2d7b45d9453e76f25efe0247bceffa616d4
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-09 16:51:24 +0500

    Solve grains task

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,36 @@
-def square(number):
-    pass
+"""Functions for calculating the number of grains of wheat on a chessboard.
+"""
 
 
-def total():
-    pass
+MIN_SQUARE = 1
+MAX_SQUARE = 64
+RATIO = 2
+
+
+def square(number: int) -> int:
+    """Calculate the number of grains on a given square.
+
+    Args:
+        number (int): Square's number.
+
+    Raises:
+        ValueError: Square must be between MIN_SQUARE and MAX_SQUARE.
+
+    Returns:
+        int: The number of grains.
+    """
+
+    if number < MIN_SQUARE or number > MAX_SQUARE:
+        raise ValueError(f"square must be between {MIN_SQUARE} and {MAX_SQUARE}")
+    
+    return RATIO ** (number - 1)
+
+
+def total() -> int:
+    """Calculate the total number of grains on the chessboard.
+
+    Returns:
+        int: The total number of grains.
+    """
+
+    return MIN_SQUARE * (1 - RATIO ** MAX_SQUARE) // (1 - RATIO)
--- python/grains/grains_test.py
+++ python/grains/grains_test.py
@@ -4,7 +4,11 @@
 
 import unittest
 
+import pytest
+
 from grains import (
+    MAX_SQUARE,
+    MIN_SQUARE,
     square,
     total,
 )
@@ -12,44 +16,47 @@ from grains import (
 
 class GrainsTest(unittest.TestCase):
 
-    def test_grains_on_square_1(self):
-        self.assertEqual(square(1), 1)
+    def test_grains_on_square_1(self) -> None:
+        assert square(1) == 1
 
-    def test_grains_on_square_2(self):
-        self.assertEqual(square(2), 2)
+    def test_grains_on_square_2(self) -> None:
+        assert square(2) == 2
 
-    def test_grains_on_square_3(self):
-        self.assertEqual(square(3), 4)
+    def test_grains_on_square_3(self) -> None:
+        assert square(3) == 4
 
-    def test_grains_on_square_4(self):
-        self.assertEqual(square(4), 8)
+    def test_grains_on_square_4(self) -> None:
+        assert square(4) == 8
 
-    def test_grains_on_square_16(self):
-        self.assertEqual(square(16), 32768)
+    def test_grains_on_square_16(self) -> None:
+        assert square(16) == 32768
 
-    def test_grains_on_square_32(self):
-        self.assertEqual(square(32), 2147483648)
+    def test_grains_on_square_32(self) -> None:
+        assert square(32) == 2147483648
 
-    def test_grains_on_square_64(self):
-        self.assertEqual(square(64), 9223372036854775808)
+    def test_grains_on_square_64(self) -> None:
+        assert square(64) == 9223372036854775808
 
-    def test_square_0_is_invalid(self):
-        with self.assertRaises(ValueError) as err:
+    def test_square_0_is_invalid(self) -> None:
+        with pytest.raises(ValueError,
+                           match=f"square must be between {MIN_SQUARE} and {MAX_SQUARE}") as err:
             square(0)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(err.exception.args[0], "square must be between 1 and 64")
+        assert isinstance(err.value, ValueError)
+        assert err.value.args[0] == "square must be between 1 and 64"
 
-    def test_negative_square_is_invalid(self):
-        with self.assertRaises(ValueError) as err:
+    def test_negative_square_is_invalid(self) -> None:
+        with pytest.raises(ValueError,
+                           match=f"square must be between {MIN_SQUARE} and {MAX_SQUARE}") as err:
             square(-1)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(err.exception.args[0], "square must be between 1 and 64")
+        assert isinstance(err.value, ValueError)
+        assert err.value.args[0] == f"square must be between {MIN_SQUARE} and {MAX_SQUARE}"
 
-    def test_square_greater_than_64_is_invalid(self):
-        with self.assertRaises(ValueError) as err:
+    def test_square_greater_than_64_is_invalid(self) -> None:
+        with pytest.raises(ValueError,
+                           match=f"square must be between {MIN_SQUARE} and {MAX_SQUARE}") as err:
             square(65)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(err.exception.args[0], "square must be between 1 and 64")
+        assert isinstance(err.value, ValueError)
+        assert err.value.args[0] == "square must be between 1 and 64"
 
-    def test_returns_the_total_number_of_grains_on_the_board(self):
-        self.assertEqual(total(), 18446744073709551615)
+    def test_returns_the_total_number_of_grains_on_the_board(self) -> None:
+        assert total() == 18446744073709551615
