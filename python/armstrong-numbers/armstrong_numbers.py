8c4bbb80062da690e00e7416caf85c84e5ec50bd
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-09 19:17:02 +0500

    Solve armstrong-numbers task

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,26 @@
-def is_armstrong_number(number):
-    pass
+"""Function for determine whether a number is an Armstrong number.
+"""
+
+
+def is_armstrong_number(number: int) -> bool:
+    """Determine whether a number is an Armstrong number.
+
+    Args:
+        number (int): Number for determine.  
+
+    Returns:
+        bool: Whether a number is an Armstrong number.
+    """
+    
+    current_number = number
+    digits: list[int] = []
+
+    while current_number > 0:
+        digit = current_number % 10
+        digits.append(digit)
+        current_number //= 10
+        
+    power = len(digits)
+    armstrong_number = sum([digit ** power for digit in digits])
+
+    return number == armstrong_number
--- python/armstrong-numbers/armstrong_numbers_test.py
+++ python/armstrong-numbers/armstrong_numbers_test.py
@@ -10,29 +10,29 @@ from armstrong_numbers import (
 
 
 class ArmstrongNumbersTest(unittest.TestCase):
-    def test_zero_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(0), True)
+    def test_zero_is_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(0) is True
 
-    def test_single_digit_numbers_are_armstrong_numbers(self):
-        self.assertIs(is_armstrong_number(5), True)
+    def test_single_digit_numbers_are_armstrong_numbers(self) -> None:
+        assert is_armstrong_number(5) is True
 
-    def test_there_are_no_two_digit_armstrong_numbers(self):
-        self.assertIs(is_armstrong_number(10), False)
+    def test_there_are_no_two_digit_armstrong_numbers(self) -> None:
+        assert is_armstrong_number(10) is False
 
-    def test_three_digit_number_that_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(153), True)
+    def test_three_digit_number_that_is_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(153) is True
 
-    def test_three_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(100), False)
+    def test_three_digit_number_that_is_not_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(100) is False
 
-    def test_four_digit_number_that_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9474), True)
+    def test_four_digit_number_that_is_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(9474) is True
 
-    def test_four_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9475), False)
+    def test_four_digit_number_that_is_not_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(9475) is False
 
-    def test_seven_digit_number_that_is_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9926315), True)
+    def test_seven_digit_number_that_is_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(9926315) is True
 
-    def test_seven_digit_number_that_is_not_an_armstrong_number(self):
-        self.assertIs(is_armstrong_number(9926314), False)
+    def test_seven_digit_number_that_is_not_an_armstrong_number(self) -> None:
+        assert is_armstrong_number(9926314) is False
