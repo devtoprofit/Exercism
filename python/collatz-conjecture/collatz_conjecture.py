ed08a93664404c95db141c1fc9ff85929f2b6901
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-09 19:54:07 +0500

    Solve collatz-conjecture task

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,33 @@
-def steps(number):
-    pass
+"""Function for determine the number of steps it takes to
+reach 1 according to the rules of the Collatz Conjecture.
+"""
+
+
+def steps(number: int) -> int:
+    """Determine the number of steps it takes to
+    reach 1 according to the rules of the Collatz Conjecture.
+
+    Args:
+        number (int): Only positive integers are allowed.
+
+    Raises:
+        ValueError: Positive integer to determine the number of steps.
+
+    Returns:
+        int: The number of steps to reach 1.
+    """
+
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+    
+    count = 0
+
+    while number > 1:
+        count += 1
+
+        if number % 2 == 0:
+            number //= 2
+        else:
+            number = number * 3 + 1
+    
+    return count
--- python/collatz-conjecture/collatz_conjecture_test.py
+++ python/collatz-conjecture/collatz_conjecture_test.py
@@ -4,32 +4,33 @@
 
 import unittest
 
+import pytest
 from collatz_conjecture import (
     steps,
 )
 
 
 class CollatzConjectureTest(unittest.TestCase):
-    def test_zero_steps_for_one(self):
-        self.assertEqual(steps(1), 0)
+    def test_zero_steps_for_one(self) -> None:
+        assert steps(1) == 0
 
-    def test_divide_if_even(self):
-        self.assertEqual(steps(16), 4)
+    def test_divide_if_even(self) -> None:
+        assert steps(16) == 4
 
-    def test_even_and_odd_steps(self):
-        self.assertEqual(steps(12), 9)
+    def test_even_and_odd_steps(self) -> None:
+        assert steps(12) == 9
 
-    def test_large_number_of_even_and_odd_steps(self):
-        self.assertEqual(steps(1000000), 152)
+    def test_large_number_of_even_and_odd_steps(self) -> None:
+        assert steps(1000000) == 152
 
-    def test_zero_is_an_error(self):
-        with self.assertRaises(ValueError) as err:
+    def test_zero_is_an_error(self) -> None:
+        with pytest.raises(ValueError, match="Only positive integers are allowed") as err:
             steps(0)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(err.exception.args[0], "Only positive integers are allowed")
+        assert isinstance(err.value, ValueError)
+        assert err.value.args[0] == "Only positive integers are allowed"
 
-    def test_negative_value_is_an_error(self):
-        with self.assertRaises(ValueError) as err:
+    def test_negative_value_is_an_error(self) -> None:
+        with pytest.raises(ValueError, match="Only positive integers are allowed") as err:
             steps(-15)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(err.exception.args[0], "Only positive integers are allowed")
+        assert isinstance(err.value, ValueError)
+        assert err.value.args[0] == "Only positive integers are allowed"
