599044c10441fb2e0ca3661614bce083f4877230
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-10 01:01:20 +0500

    Solve leap task

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,15 @@
-def leap_year(year):
-    pass
+"""Function to determine whether a given year is a leap year.
+"""
+
+
+def leap_year(year: int) -> bool:
+    """Determine whether a given year is a leap year.
+
+    Args:
+        year (int): Year for check.
+
+    Returns:
+        bool: Whether a given year is a leap year.
+    """
+    
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
