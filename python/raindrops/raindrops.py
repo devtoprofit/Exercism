081dee583302f223218e40a43764d76beac44928
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-11 01:51:57 +0500

    Solve raindrops task

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,29 @@
-def convert(number):
-    pass
+"""Function to convert a number into its corresponding raindrop sounds.
+"""
+
+
+def convert(number: int) -> str:
+    """Convert a number into its corresponding raindrop sounds.
+
+    Args:
+        number (int): number for convert
+
+    Returns:
+        str: _description_
+    """
+
+    result = ""
+
+    if number % 3 == 0:
+        result += "Pling"
+    
+    if number % 5 == 0:
+        result += "Plang"
+    
+    if number % 7 == 0:
+        result += "Plong"
+    
+    if not result:
+        result = str(number)
+    
+    return result
