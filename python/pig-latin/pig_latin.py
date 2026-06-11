2c82ad7075ca1601b1b21ac6d481c2b373dedc65
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-11 17:19:14 +0500

    Solve pig-latin task

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,51 @@
-def translate(text):
-    pass
+"""Functions to translate text from English to Pig Latin.
+"""
+
+
+SUFFIX = "ay"
+VOWELS = ["a", "e", "i", "o", "u"]
+
+
+def translate_word(word: str) -> str:
+    """Translate word from English to Pig Latin.
+
+    Args:
+        word (str): English word.
+
+    Returns:
+        str: Word in Pig Latin.
+    """
+
+    if word.startswith(("xr", "yt")):
+        return word + SUFFIX
+    
+    prefix = ""
+    prefix_length = 0
+
+    for letter in word:
+        
+        if letter == "u" and prefix.endswith("q"):
+            prefix_length += 1
+            break
+
+        if (letter == "y" and prefix_length > 0) or letter in VOWELS:
+            break
+
+        prefix += letter
+        prefix_length += 1
+
+    return word[prefix_length:] + word[:prefix_length] + SUFFIX
+
+
+def translate(text: str) -> str:
+    """Translate text from English to Pig Latin.
+
+    Args:
+        word (str): English text.
+
+    Returns:
+        str: Text in Pig Latin.
+    """
+
+    words = text.split()
+    return " ".join([translate_word(word) for word in words])
--- python/pig-latin/pig_latin_test.py
+++ python/pig-latin/pig_latin_test.py
@@ -11,71 +11,71 @@ from pig_latin import (
 
 class PigLatinTest(unittest.TestCase):
 
-    def test_word_beginning_with_a(self):
-        self.assertEqual(translate("apple"), "appleay")
+    def test_word_beginning_with_a(self) -> None:
+        assert translate("apple") == "appleay"
 
-    def test_word_beginning_with_e(self):
-        self.assertEqual(translate("ear"), "earay")
+    def test_word_beginning_with_e(self) -> None:
+        assert translate("ear") == "earay"
 
-    def test_word_beginning_with_i(self):
-        self.assertEqual(translate("igloo"), "iglooay")
+    def test_word_beginning_with_i(self) -> None:
+        assert translate("igloo") == "iglooay"
 
-    def test_word_beginning_with_o(self):
-        self.assertEqual(translate("object"), "objectay")
+    def test_word_beginning_with_o(self) -> None:
+        assert translate("object") == "objectay"
 
-    def test_word_beginning_with_u(self):
-        self.assertEqual(translate("under"), "underay")
+    def test_word_beginning_with_u(self) -> None:
+        assert translate("under") == "underay"
 
-    def test_word_beginning_with_a_vowel_and_followed_by_a_qu(self):
-        self.assertEqual(translate("equal"), "equalay")
+    def test_word_beginning_with_a_vowel_and_followed_by_a_qu(self) -> None:
+        assert translate("equal") == "equalay"
 
-    def test_word_beginning_with_p(self):
-        self.assertEqual(translate("pig"), "igpay")
+    def test_word_beginning_with_p(self) -> None:
+        assert translate("pig") == "igpay"
 
-    def test_word_beginning_with_k(self):
-        self.assertEqual(translate("koala"), "oalakay")
+    def test_word_beginning_with_k(self) -> None:
+        assert translate("koala") == "oalakay"
 
-    def test_word_beginning_with_x(self):
-        self.assertEqual(translate("xenon"), "enonxay")
+    def test_word_beginning_with_x(self) -> None:
+        assert translate("xenon") == "enonxay"
 
-    def test_word_beginning_with_q_without_a_following_u(self):
-        self.assertEqual(translate("qat"), "atqay")
+    def test_word_beginning_with_q_without_a_following_u(self) -> None:
+        assert translate("qat") == "atqay"
 
-    def test_word_beginning_with_consonant_and_vowel_containing_qu(self):
-        self.assertEqual(translate("liquid"), "iquidlay")
+    def test_word_beginning_with_consonant_and_vowel_containing_qu(self) -> None:
+        assert translate("liquid") == "iquidlay"
 
-    def test_word_beginning_with_ch(self):
-        self.assertEqual(translate("chair"), "airchay")
+    def test_word_beginning_with_ch(self) -> None:
+        assert translate("chair") == "airchay"
 
-    def test_word_beginning_with_qu(self):
-        self.assertEqual(translate("queen"), "eenquay")
+    def test_word_beginning_with_qu(self) -> None:
+        assert translate("queen") == "eenquay"
 
-    def test_word_beginning_with_qu_and_a_preceding_consonant(self):
-        self.assertEqual(translate("square"), "aresquay")
+    def test_word_beginning_with_qu_and_a_preceding_consonant(self) -> None:
+        assert translate("square") == "aresquay"
 
-    def test_word_beginning_with_th(self):
-        self.assertEqual(translate("therapy"), "erapythay")
+    def test_word_beginning_with_th(self) -> None:
+        assert translate("therapy") == "erapythay"
 
-    def test_word_beginning_with_thr(self):
-        self.assertEqual(translate("thrush"), "ushthray")
+    def test_word_beginning_with_thr(self) -> None:
+        assert translate("thrush") == "ushthray"
 
-    def test_word_beginning_with_sch(self):
-        self.assertEqual(translate("school"), "oolschay")
+    def test_word_beginning_with_sch(self) -> None:
+        assert translate("school") == "oolschay"
 
-    def test_word_beginning_with_yt(self):
-        self.assertEqual(translate("yttria"), "yttriaay")
+    def test_word_beginning_with_yt(self) -> None:
+        assert translate("yttria") == "yttriaay"
 
-    def test_word_beginning_with_xr(self):
-        self.assertEqual(translate("xray"), "xrayay")
+    def test_word_beginning_with_xr(self) -> None:
+        assert translate("xray") == "xrayay"
 
-    def test_y_is_treated_like_a_consonant_at_the_beginning_of_a_word(self):
-        self.assertEqual(translate("yellow"), "ellowyay")
+    def test_y_is_treated_like_a_consonant_at_the_beginning_of_a_word(self) -> None:
+        assert translate("yellow") == "ellowyay"
 
-    def test_y_is_treated_like_a_vowel_at_the_end_of_a_consonant_cluster(self):
-        self.assertEqual(translate("rhythm"), "ythmrhay")
+    def test_y_is_treated_like_a_vowel_at_the_end_of_a_consonant_cluster(self) -> None:
+        assert translate("rhythm") == "ythmrhay"
 
-    def test_y_as_second_letter_in_two_letter_word(self):
-        self.assertEqual(translate("my"), "ymay")
+    def test_y_as_second_letter_in_two_letter_word(self) -> None:
+        assert translate("my") == "ymay"
 
-    def test_a_whole_phrase(self):
-        self.assertEqual(translate("quick fast run"), "ickquay astfay unray")
+    def test_a_whole_phrase(self) -> None:
+        assert translate("quick fast run") == "ickquay astfay unray"
