7348fca55b93e09c21dbfa8364ce06e3c00c9044
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-12 23:51:08 +0500

    Solve pangram task

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,15 @@
-def is_pangram(sentence):
-    pass
+"""Function for determine if a sentence is a pangram.
+"""
+
+
+def is_pangram(sentence: str) -> bool:
+    """Determine if a sentence is a pangram.
+
+    Args:
+        sentence (str): Sentence for check.
+
+    Returns:
+        bool: True - if a sentence is a pangram, otherwise False.
+    """
+
+    return len({char for char in sentence.lower() if char.isalpha()}) == 26
