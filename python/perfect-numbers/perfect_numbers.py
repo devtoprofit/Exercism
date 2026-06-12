65637299eaeb7f0df92dd28f0c6023465bb1fd24
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-12 19:58:08 +0500

    Solve perfect-numbers task

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,24 @@
-def classify(number):
+"""Function for determine if a number is perfect.
+"""
+
+
+def classify(number: int) -> str:
     """ A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+    
+    aliquot_sum = sum(num for num in range(1, number) if number % num == 0)
+
+    if number == aliquot_sum:
+        category = "perfect"
+    elif number < aliquot_sum:
+        category = "abundant"
+    else:
+        category = "deficient"
+
+    return category
--- python/perfect-numbers/perfect_numbers_test.py
+++ python/perfect-numbers/perfect_numbers_test.py
@@ -4,68 +4,65 @@
 
 import unittest
 
+import pytest
 from perfect_numbers import (
     classify,
 )
 
 
 class PerfectNumbersTest(unittest.TestCase):
-    def test_smallest_perfect_number_is_classified_correctly(self):
-        self.assertEqual(classify(6), "perfect")
+    def test_smallest_perfect_number_is_classified_correctly(self) -> None:
+        assert classify(6) == "perfect"
 
-    def test_medium_perfect_number_is_classified_correctly(self):
-        self.assertEqual(classify(28), "perfect")
+    def test_medium_perfect_number_is_classified_correctly(self) -> None:
+        assert classify(28) == "perfect"
 
-    def test_large_perfect_number_is_classified_correctly(self):
-        self.assertEqual(classify(33550336), "perfect")
+    def test_large_perfect_number_is_classified_correctly(self) -> None:
+        assert classify(33550336) == "perfect"
 
 
 class AbundantNumbersTest(unittest.TestCase):
-    def test_smallest_abundant_number_is_classified_correctly(self):
-        self.assertEqual(classify(12), "abundant")
+    def test_smallest_abundant_number_is_classified_correctly(self) -> None:
+        assert classify(12) == "abundant"
 
-    def test_medium_abundant_number_is_classified_correctly(self):
-        self.assertEqual(classify(30), "abundant")
+    def test_medium_abundant_number_is_classified_correctly(self) -> None:
+        assert classify(30) == "abundant"
 
-    def test_large_abundant_number_is_classified_correctly(self):
-        self.assertEqual(classify(33550335), "abundant")
+    def test_large_abundant_number_is_classified_correctly(self) -> None:
+        assert classify(33550335) == "abundant"
 
-    def test_perfect_square_abundant_number_is_classified_correctly(self):
-        self.assertEqual(classify(196), "abundant")
+    def test_perfect_square_abundant_number_is_classified_correctly(self) -> None:
+        assert classify(196) == "abundant"
 
 
 class DeficientNumbersTest(unittest.TestCase):
-    def test_smallest_prime_deficient_number_is_classified_correctly(self):
-        self.assertEqual(classify(2), "deficient")
+    def test_smallest_prime_deficient_number_is_classified_correctly(self) -> None:
+        assert classify(2) == "deficient"
 
-    def test_smallest_non_prime_deficient_number_is_classified_correctly(self):
-        self.assertEqual(classify(4), "deficient")
+    def test_smallest_non_prime_deficient_number_is_classified_correctly(self) -> None:
+        assert classify(4) == "deficient"
 
-    def test_medium_deficient_number_is_classified_correctly(self):
-        self.assertEqual(classify(32), "deficient")
+    def test_medium_deficient_number_is_classified_correctly(self) -> None:
+        assert classify(32) == "deficient"
 
-    def test_large_deficient_number_is_classified_correctly(self):
-        self.assertEqual(classify(33550337), "deficient")
+    def test_large_deficient_number_is_classified_correctly(self) -> None:
+        assert classify(33550337) == "deficient"
 
-    def test_edge_case_no_factors_other_than_itself_is_classified_correctly(self):
-        self.assertEqual(classify(1), "deficient")
+    def test_edge_case_no_factors_other_than_itself_is_classified_correctly(self) -> None:
+        assert classify(1) == "deficient"
 
 
 class InvalidInputsTest(unittest.TestCase):
-    def test_zero_is_rejected_as_it_is_not_a_positive_integer(self):
-        with self.assertRaises(ValueError) as err:
+    def test_zero_is_rejected_as_it_is_not_a_positive_integer(self) -> None:
+        with pytest.raises(ValueError,
+                           match="Classification is only possible for positive integers") as err:
             classify(0)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(
-            err.exception.args[0],
-            "Classification is only possible for positive integers.",
-        )
+        assert type(err.value) == ValueError  # noqa: E721
+        assert err.value.args[0] == "Classification is only possible for positive integers."
 
-    def test_negative_integer_is_rejected_as_it_is_not_a_positive_integer(self):
-        with self.assertRaises(ValueError) as err:
+    def test_negative_integer_is_rejected_as_it_is_not_a_positive_integer(self) -> None:
+        with pytest.raises(ValueError,
+                           match="Classification is only possible for positive integers") as err:
             classify(-1)
-        self.assertEqual(type(err.exception), ValueError)
-        self.assertEqual(
-            err.exception.args[0],
-            "Classification is only possible for positive integers.",
-        )
+        assert type(err.value) == ValueError  # noqa: E721
+        assert err.value.args[0] == "Classification is only possible for positive integers."
