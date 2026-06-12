3120557871196a334a3d8639da9fd7dc25d0ebca
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-13 00:19:49 +0500

    Solve isogram task

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,16 @@
-def is_isogram(string):
-    pass
+"""Function for determine if a word or phrase is an isogram.
+"""
+
+
+def is_isogram(string: str) -> bool:
+    """Determine if a word or phrase is an isogram.
+
+    Args:
+        string (str): Word or phrase for check.
+
+    Returns:
+        bool: True - if a word is a isogram, otherwise False.
+    """
+
+    letters = string.replace(" ", "").replace("-", "").lower()
+    return len(letters) == len(set(letters))
--- python/isogram/isogram_test.py
+++ python/isogram/isogram_test.py
@@ -10,44 +10,44 @@ from isogram import (
 
 
 class IsogramTest(unittest.TestCase):
-    def test_empty_string(self):
-        self.assertIs(is_isogram(""), True)
+    def test_empty_string(self) -> None:
+        assert is_isogram("") is True
 
-    def test_isogram_with_only_lower_case_characters(self):
-        self.assertIs(is_isogram("isogram"), True)
+    def test_isogram_with_only_lower_case_characters(self) -> None:
+        assert is_isogram("isogram") is True
 
-    def test_word_with_one_duplicated_character(self):
-        self.assertIs(is_isogram("eleven"), False)
+    def test_word_with_one_duplicated_character(self) -> None:
+        assert is_isogram("eleven") is False
 
-    def test_word_with_one_duplicated_character_from_the_end_of_the_alphabet(self):
-        self.assertIs(is_isogram("zzyzx"), False)
+    def test_word_with_one_duplicated_character_from_the_end_of_the_alphabet(self) -> None:
+        assert is_isogram("zzyzx") is False
 
-    def test_longest_reported_english_isogram(self):
-        self.assertIs(is_isogram("subdermatoglyphic"), True)
+    def test_longest_reported_english_isogram(self) -> None:
+        assert is_isogram("subdermatoglyphic") is True
 
-    def test_word_with_duplicated_character_in_mixed_case(self):
-        self.assertIs(is_isogram("Alphabet"), False)
+    def test_word_with_duplicated_character_in_mixed_case(self) -> None:
+        assert is_isogram("Alphabet") is False
 
-    def test_word_with_duplicated_character_in_mixed_case_lowercase_first(self):
-        self.assertIs(is_isogram("alphAbet"), False)
+    def test_word_with_duplicated_character_in_mixed_case_lowercase_first(self) -> None:
+        assert is_isogram("alphAbet") is False
 
-    def test_hypothetical_isogrammic_word_with_hyphen(self):
-        self.assertIs(is_isogram("thumbscrew-japingly"), True)
+    def test_hypothetical_isogrammic_word_with_hyphen(self) -> None:
+        assert is_isogram("thumbscrew-japingly") is True
 
-    def test_hypothetical_word_with_duplicated_character_following_hyphen(self):
-        self.assertIs(is_isogram("thumbscrew-jappingly"), False)
+    def test_hypothetical_word_with_duplicated_character_following_hyphen(self) -> None:
+        assert is_isogram("thumbscrew-jappingly") is False
 
-    def test_isogram_with_duplicated_hyphen(self):
-        self.assertIs(is_isogram("six-year-old"), True)
+    def test_isogram_with_duplicated_hyphen(self) -> None:
+        assert is_isogram("six-year-old") is True
 
-    def test_made_up_name_that_is_an_isogram(self):
-        self.assertIs(is_isogram("Emily Jung Schwartzkopf"), True)
+    def test_made_up_name_that_is_an_isogram(self) -> None:
+        assert is_isogram("Emily Jung Schwartzkopf") is True
 
-    def test_duplicated_character_in_the_middle(self):
-        self.assertIs(is_isogram("accentor"), False)
+    def test_duplicated_character_in_the_middle(self) -> None:
+        assert is_isogram("accentor") is False
 
-    def test_same_first_and_last_characters(self):
-        self.assertIs(is_isogram("angola"), False)
+    def test_same_first_and_last_characters(self) -> None:
+        assert is_isogram("angola") is False
 
-    def test_word_with_duplicated_character_and_with_two_hyphens(self):
-        self.assertIs(is_isogram("up-to-date"), False)
+    def test_word_with_duplicated_character_and_with_two_hyphens(self) -> None:
+        assert is_isogram("up-to-date") is False
