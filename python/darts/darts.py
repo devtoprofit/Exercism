485d0d100d7156b6905a8997d063cedf88123af4
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-12 14:30:28 +0500

    Solve darts task

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,27 @@
-def score(x, y):
-    pass
+"""Function for calculate the points scored in a single toss of a Darts game.
+"""
+
+
+def score(x: float, y: float) -> int:
+    """Calculate the points scored in a single toss of a Darts game.
+
+    Args:
+        x (float): Coordinate.
+        y (float): Coordinate.
+
+    Returns:
+        int: Scored points.
+    """
+
+    radius = (x ** 2 + y ** 2) ** 0.5
+
+    if radius <= 1:
+        points = 10
+    elif radius <= 5:
+        points = 5
+    elif radius <= 10:
+        points = 1
+    else:
+        points = 0
+
+    return points
