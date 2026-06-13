e52c750f513c8aaa002255ca8ecf80127ca78ef4
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-13 11:36:27 +0500

    Solve isbn-verifier task

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,31 @@
-def is_valid(isbn):
-    pass
+"""Function for validate book identification numbers.
+"""
+
+
+def is_valid(isbn: str) -> bool:
+    """Validate book identification numbers.
+
+    Args:
+        isbn (str): ISBN for check.
+
+    Returns:
+        bool: True - if ISBN is valid, otherwise False.
+    """
+
+    clean_isbn = isbn.replace("-", "")
+
+    if len(clean_isbn) != 10:
+        return False
+    
+    check_sum = 0
+
+    for factor, letter in enumerate(clean_isbn[:: -1], 1):
+        
+        digit = "10" if factor == 1 and letter == "X" else letter
+        
+        if not digit.isdigit():
+            return False
+        
+        check_sum += int(digit) * factor
+    
+    return check_sum % 11 == 0
