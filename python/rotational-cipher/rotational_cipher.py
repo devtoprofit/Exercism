12d72a4cf68d45f871644a6d5774729c24a11871
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-13 13:28:24 +0500

    Solve rotational-cipher task

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,23 @@
-def rotate(text, key):
-    pass
+"""Implementation of the rotational cipher, also sometimes called the Caesar cipher.
+"""
+
+from string import ascii_lowercase, ascii_uppercase
+
+
+def rotate(text: str, key: int) -> str:
+    """Rotational cipher.
+
+    Args:
+        text (str): Text to transform.
+        key (int): The letter is shifted for as many values as the value of the key.
+
+    Returns:
+        str: Encoded text.
+    """
+
+    alphabet = ascii_uppercase + ascii_lowercase
+    rot_alphabet = (ascii_uppercase[key:] + ascii_uppercase[:key]
+                      + ascii_lowercase[key:] + ascii_lowercase[:key])
+    translation_table = str.maketrans(alphabet, rot_alphabet)
+
+    return text.translate(translation_table)
