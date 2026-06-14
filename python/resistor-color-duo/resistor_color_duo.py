cc2a76c64a35c218c14497b3665c8b11f857108e
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-15 02:43:05 +0500

    Solve resistor-color-duo task

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,28 @@
-def value(colors):
-    pass
+"""Function for represention colors as number.
+"""
+
+COLORS_BAND = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+    ]
+
+
+def value(colors: list[str]) -> int:
+    """Function takes color names as input and output a two digit number,
+    even if the input is more than two colors!
+
+    Args:
+        colors (list[str]): Colors.
+
+    Returns:
+        int: A number representing value of colors.
+    """
+    return COLORS_BAND.index(colors[0]) * 10 + COLORS_BAND.index(colors[1])
--- python/resistor-color-duo/resistor_color_duo_test.py
+++ python/resistor-color-duo/resistor_color_duo_test.py
@@ -10,23 +10,23 @@ from resistor_color_duo import (
 
 
 class ResistorColorDuoTest(unittest.TestCase):
-    def test_brown_and_black(self):
-        self.assertEqual(value(["brown", "black"]), 10)
+    def test_brown_and_black(self) -> None:
+        assert value(["brown", "black"]) == 10
 
-    def test_blue_and_grey(self):
-        self.assertEqual(value(["blue", "grey"]), 68)
+    def test_blue_and_grey(self) -> None:
+        assert value(["blue", "grey"]) == 68
 
-    def test_yellow_and_violet(self):
-        self.assertEqual(value(["yellow", "violet"]), 47)
+    def test_yellow_and_violet(self) -> None:
+        assert value(["yellow", "violet"]) == 47
 
-    def test_white_and_red(self):
-        self.assertEqual(value(["white", "red"]), 92)
+    def test_white_and_red(self) -> None:
+        assert value(["white", "red"]) == 92
 
-    def test_orange_and_orange(self):
-        self.assertEqual(value(["orange", "orange"]), 33)
+    def test_orange_and_orange(self) -> None:
+        assert value(["orange", "orange"]) == 33
 
-    def test_ignore_additional_colors(self):
-        self.assertEqual(value(["green", "brown", "orange"]), 51)
+    def test_ignore_additional_colors(self) -> None:
+        assert value(["green", "brown", "orange"]) == 51
 
-    def test_black_and_brown_one_digit(self):
-        self.assertEqual(value(["black", "brown"]), 1)
+    def test_black_and_brown_one_digit(self) -> None:
+        assert value(["black", "brown"]) == 1
