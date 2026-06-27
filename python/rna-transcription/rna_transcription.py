9247c53ef57d4a36d344539daa09e83e5157f7cd
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-27 16:55:49 +0500

    Solve rna-transcription task

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,18 @@
-def to_rna(dna_strand):
-    pass
+"""Function for determine the RNA complement of a given DNA sequence.
+"""
+
+def to_rna(dna_strand: str) -> str:
+    """Determine the RNA complement of a given DNA sequence.
+
+    Args:
+        dna_strand (str): The DNA sequence.
+
+    Returns:
+        str: The RNA complement of a given DNA sequence.
+    """
+
+    dna = "GCTA"
+    rna = "CGAU"
+
+    translation_table = str.maketrans(dna, rna)
+    return dna_strand.translate(translation_table)
--- python/rna-transcription/rna_transcription_test.py
+++ python/rna-transcription/rna_transcription_test.py
@@ -10,20 +10,20 @@ from rna_transcription import (
 
 
 class RnaTranscriptionTest(unittest.TestCase):
-    def test_empty_rna_sequence(self):
-        self.assertEqual(to_rna(""), "")
+    def test_empty_rna_sequence(self) -> None:
+        assert to_rna("") == ""
 
-    def test_rna_complement_of_cytosine_is_guanine(self):
-        self.assertEqual(to_rna("C"), "G")
+    def test_rna_complement_of_cytosine_is_guanine(self) -> None:
+        assert to_rna("C") == "G"
 
-    def test_rna_complement_of_guanine_is_cytosine(self):
-        self.assertEqual(to_rna("G"), "C")
+    def test_rna_complement_of_guanine_is_cytosine(self) -> None:
+        assert to_rna("G") == "C"
 
-    def test_rna_complement_of_thymine_is_adenine(self):
-        self.assertEqual(to_rna("T"), "A")
+    def test_rna_complement_of_thymine_is_adenine(self) -> None:
+        assert to_rna("T") == "A"
 
-    def test_rna_complement_of_adenine_is_uracil(self):
-        self.assertEqual(to_rna("A"), "U")
+    def test_rna_complement_of_adenine_is_uracil(self) -> None:
+        assert to_rna("A") == "U"
 
-    def test_rna_complement(self):
-        self.assertEqual(to_rna("ACGTGGTCTTAA"), "UGCACCAGAAUU")
+    def test_rna_complement(self) -> None:
+        assert to_rna("ACGTGGTCTTAA") == "UGCACCAGAAUU"
