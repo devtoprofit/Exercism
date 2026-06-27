6e4ddbdc9d5d2b1d44429712d3f82b1b39bce553
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-27 16:39:45 +0500

    Solve matching-brackets task

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,32 @@
-def is_paired(input_string):
-    pass
+"""Function for verify that any and all brackets pairs are matched and nested correctly. 
+"""
+
+def is_paired(input_string: str) -> bool:
+    """Function verify that any and all brackets pairs are matched and nested correctly. 
+
+    Args:
+        input_string (str): A string for check.
+
+    Returns:
+        bool: A given string has all brackets pairs are matched and nested correctly.
+    """
+
+    brackets = {"[": "]", "{": "}", "(": ")"}
+    close_brackets = brackets.values()
+    stack: list[str] = []
+
+    for symbol in input_string:
+        if symbol in brackets:
+            stack.append(symbol)
+
+        elif symbol in close_brackets:
+
+            if len(stack) == 0:
+                return False
+            
+            open_bracket = stack.pop()
+
+            if symbol != brackets[open_bracket]:
+                return False
+    
+    return len(stack) == 0
--- python/matching-brackets/matching_brackets_test.py
+++ python/matching-brackets/matching_brackets_test.py
@@ -10,67 +10,64 @@ from matching_brackets import (
 
 
 class MatchingBracketsTest(unittest.TestCase):
-    def test_paired_square_brackets(self):
-        self.assertEqual(is_paired("[]"), True)
+    def test_paired_square_brackets(self) -> None:
+        assert is_paired("[]")
 
-    def test_empty_string(self):
-        self.assertEqual(is_paired(""), True)
+    def test_empty_string(self) -> None:
+        assert is_paired("")
 
-    def test_unpaired_brackets(self):
-        self.assertEqual(is_paired("[["), False)
+    def test_unpaired_brackets(self) -> None:
+        assert not is_paired("[[")
 
-    def test_wrong_ordered_brackets(self):
-        self.assertEqual(is_paired("}{"), False)
+    def test_wrong_ordered_brackets(self) -> None:
+        assert not is_paired("}{")
 
-    def test_wrong_closing_bracket(self):
-        self.assertEqual(is_paired("{]"), False)
+    def test_wrong_closing_bracket(self) -> None:
+        assert not is_paired("{]")
 
-    def test_paired_with_whitespace(self):
-        self.assertEqual(is_paired("{ }"), True)
+    def test_paired_with_whitespace(self) -> None:
+        assert is_paired("{ }")
 
-    def test_partially_paired_brackets(self):
-        self.assertEqual(is_paired("{[])"), False)
+    def test_partially_paired_brackets(self) -> None:
+        assert not is_paired("{[])")
 
-    def test_simple_nested_brackets(self):
-        self.assertEqual(is_paired("{[]}"), True)
+    def test_simple_nested_brackets(self) -> None:
+        assert is_paired("{[]}")
 
-    def test_several_paired_brackets(self):
-        self.assertEqual(is_paired("{}[]"), True)
+    def test_several_paired_brackets(self) -> None:
+        assert is_paired("{}[]")
 
-    def test_paired_and_nested_brackets(self):
-        self.assertEqual(is_paired("([{}({}[])])"), True)
+    def test_paired_and_nested_brackets(self) -> None:
+        assert is_paired("([{}({}[])])")
 
-    def test_unopened_closing_brackets(self):
-        self.assertEqual(is_paired("{[)][]}"), False)
+    def test_unopened_closing_brackets(self) -> None:
+        assert not is_paired("{[)][]}")
 
-    def test_unpaired_and_nested_brackets(self):
-        self.assertEqual(is_paired("([{])"), False)
+    def test_unpaired_and_nested_brackets(self) -> None:
+        assert not is_paired("([{])")
 
-    def test_paired_and_wrong_nested_brackets(self):
-        self.assertEqual(is_paired("[({]})"), False)
+    def test_paired_and_wrong_nested_brackets(self) -> None:
+        assert not is_paired("[({]})")
 
-    def test_paired_and_wrong_nested_brackets_but_innermost_are_correct(self):
-        self.assertEqual(is_paired("[({}])"), False)
+    def test_paired_and_wrong_nested_brackets_but_innermost_are_correct(self) -> None:
+        assert not is_paired("[({}])")
 
-    def test_paired_and_incomplete_brackets(self):
-        self.assertEqual(is_paired("{}["), False)
+    def test_paired_and_incomplete_brackets(self) -> None:
+        assert not is_paired("{}[")
 
-    def test_too_many_closing_brackets(self):
-        self.assertEqual(is_paired("[]]"), False)
+    def test_too_many_closing_brackets(self) -> None:
+        assert not is_paired("[]]")
 
-    def test_early_unexpected_brackets(self):
-        self.assertEqual(is_paired(")()"), False)
+    def test_early_unexpected_brackets(self) -> None:
+        assert not is_paired(")()")
 
-    def test_early_mismatched_brackets(self):
-        self.assertEqual(is_paired("{)()"), False)
+    def test_early_mismatched_brackets(self) -> None:
+        assert not is_paired("{)()")
 
-    def test_math_expression(self):
-        self.assertEqual(is_paired("(((185 + 223.85) * 15) - 543)/2"), True)
+    def test_math_expression(self) -> None:
+        assert is_paired("(((185 + 223.85) * 15) - 543)/2")
 
-    def test_complex_latex_expression(self):
-        self.assertEqual(
-            is_paired(
-                "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)"
-            ),
-            True,
-        )
+    def test_complex_latex_expression(self) -> None:
+        assert is_paired(
+            "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ "
+            "\\mathrm{e}^{x} &... x^2 \\end{array}\\right)")
