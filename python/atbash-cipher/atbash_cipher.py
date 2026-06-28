2a158d4c27aac26c9b3697c0619e8f7bbda01ceb
Andrey Shelyagin <devtoprofit@gmail.com>
2026-06-28 13:28:54 +0500

    Solve atbash-cipher task

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,46 @@
-def encode(plain_text):
-    pass
+"""An implementation of the Atbash cipher,
+an ancient encryption system created in the Middle East.
+"""
+
+from string import ascii_lowercase
+
+PLAIN = ascii_lowercase
+CIPHER = ascii_lowercase[::-1]
 
 
-def decode(ciphered_text):
-    pass
+def encode(plain_text: str) -> str:
+    """Encode a given text.
+
+    Args:
+        plain_text (str): Text for encode.
+
+    Returns:
+        str: Encoded text.
+    """
+
+    text = ""
+
+    for letter in plain_text.lower():
+        if letter.isalnum():
+            text += letter
+
+    translation_table = str.maketrans(PLAIN, CIPHER)
+    text = text.translate(translation_table)
+    
+    return " ".join([text[index: index + 5] for index in range(0, len(text), 5)])
+
+
+def decode(ciphered_text: str) -> str:
+    """Decode a given text.
+
+    Args:
+        ciphered_text (str): Text for decode.
+
+    Returns:
+        str: Decoded text.
+    """
+    
+    translation_table = str.maketrans(CIPHER, PLAIN)
+    text = ciphered_text.replace(" ", "")
+
+    return text.translate(translation_table)
--- python/atbash-cipher/atbash_cipher_test.py
+++ python/atbash-cipher/atbash_cipher_test.py
@@ -11,55 +11,46 @@ from atbash_cipher import (
 
 
 class AtbashCipherTest(unittest.TestCase):
-    def test_encode_yes(self):
-        self.assertEqual(encode("yes"), "bvh")
+    def test_encode_yes(self) -> None:
+        assert encode("yes") == "bvh"
 
-    def test_encode_no(self):
-        self.assertEqual(encode("no"), "ml")
+    def test_encode_no(self) -> None:
+        assert encode("no") == "ml"
 
-    def test_encode_omg(self):
-        self.assertEqual(encode("OMG"), "lnt")
+    def test_encode_omg(self) -> None:
+        assert encode("OMG") == "lnt"
 
-    def test_encode_spaces(self):
-        self.assertEqual(encode("O M G"), "lnt")
+    def test_encode_spaces(self) -> None:
+        assert encode("O M G") == "lnt"
 
-    def test_encode_mindblowingly(self):
-        self.assertEqual(encode("mindblowingly"), "nrmwy oldrm tob")
+    def test_encode_mindblowingly(self) -> None:
+        assert encode("mindblowingly") == "nrmwy oldrm tob"
 
-    def test_encode_numbers(self):
-        self.assertEqual(encode("Testing,1 2 3, testing."), "gvhgr mt123 gvhgr mt")
+    def test_encode_numbers(self) -> None:
+        assert encode("Testing,1 2 3, testing.") == "gvhgr mt123 gvhgr mt"
 
-    def test_encode_deep_thought(self):
-        self.assertEqual(encode("Truth is fiction."), "gifgs rhurx grlm")
+    def test_encode_deep_thought(self) -> None:
+        assert encode("Truth is fiction.") == "gifgs rhurx grlm"
 
-    def test_encode_all_the_letters(self):
-        self.assertEqual(
-            encode("The quick brown fox jumps over the lazy dog."),
-            "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt",
-        )
+    def test_encode_all_the_letters(self) -> None:
+        assert encode("The quick brown fox jumps over the lazy dog."
+                      ) == "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
 
-    def test_decode_exercism(self):
-        self.assertEqual(decode("vcvix rhn"), "exercism")
+    def test_decode_exercism(self) -> None:
+        assert decode("vcvix rhn") == "exercism"
 
-    def test_decode_a_sentence(self):
-        self.assertEqual(
-            decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"),
-            "anobstacleisoftenasteppingstone",
-        )
+    def test_decode_a_sentence(self) -> None:
+        assert decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v") == "anobstacleisoftenasteppingstone"
 
-    def test_decode_numbers(self):
-        self.assertEqual(decode("gvhgr mt123 gvhgr mt"), "testing123testing")
+    def test_decode_numbers(self) -> None:
+        assert decode("gvhgr mt123 gvhgr mt") == "testing123testing"
 
-    def test_decode_all_the_letters(self):
-        self.assertEqual(
-            decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"),
-            "thequickbrownfoxjumpsoverthelazydog",
-        )
+    def test_decode_all_the_letters(self) -> None:
+        assert decode("gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
+                      ) == "thequickbrownfoxjumpsoverthelazydog"
 
-    def test_decode_with_too_many_spaces(self):
-        self.assertEqual(decode("vc vix    r hn"), "exercism")
+    def test_decode_with_too_many_spaces(self) -> None:
+        assert decode("vc vix    r hn") == "exercism"
 
-    def test_decode_with_no_spaces(self):
-        self.assertEqual(
-            decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv"), "anobstacleisoftenasteppingstone"
-        )
+    def test_decode_with_no_spaces(self) -> None:
+        assert decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv") == "anobstacleisoftenasteppingstone"
